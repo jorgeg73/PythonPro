5b0a5c5171708c2b0d5906962b60ca3c109427fe
Jorgeg73 <jorge.gomez73@gmail.com>
2017-04-21 12:15:29 -0700

    Opweraciones

--- Operaciones.py
+++ Operaciones.py
@@ -3,34 +3,37 @@ print "Programa para multiplicar, Suma, Resta, Division"
 print ("Selecciona un numero\n 1. Suma\n 2. Resta\n 3. Multiplicacion\n 4. Division\n")
 
 opcion = input()
-while opcion <=
-if opcion == 1:
-    print "Suma"
-    x = input("Primer numero: ")
-    y = input ("Segundo numero: ")
-    suma = int(x + y)
-    print x, "+", y, "=", suma
+#while (opcion <=4):
+    if opcion == 1:
+        print "Suma"
+        x = input("Primer numero: ")
+        y = input ("Segundo numero: ")
+        suma = int(x + y)
+        print x, "+", y, "=", suma
 
-elif opcion == 2:
-    print "Resta"
-    x = input("Primer numero: ")
-    y = input("Segundo numero: ")
-    res = int(x - y)
-    print x, "-", y, "=", res
+    elif opcion == 2:
+        print "Resta"
+        x = input("Primer numero: ")
+        y = input("Segundo numero: ")
+        res = int(x - y)
+        print x, "-", y, "=", res
 
-elif opcion == 3:
-    print "Multiplicacion"
-    x = input("Primer numero: ")
-    y = input("Segundo numero: ")
-    mul = int(x * y)
-    print x, "*", y, "=", mul
+    elif opcion == 3:
+        print "Multiplicacion"
+        x = input("Primer numero: ")
+        y = input("Segundo numero: ")
+        mul = int(x * y)
+        print x, "*", y, "=", mul
+        break
 
-elif opcion == 4:
-    print "Division"
-    x = input("Primer numero: ")
-    y = input("Segundo numero: ")
-    div = int(x / y)
-    print x, "/", y, "=", div
+    elif opcion == 4:
+        print "Division"
+        x = input("Primer numero: ")
+        y = input("Segundo numero: ")
+        div = int(x / y)
+        print x, "/", y, "=", div
 
-else:
-    print "No es opcion Valida"
+    else:
+        print "No es opcion Valida"
+
+#print "AQuieres otra operacion"
