8b0f97b6a918096bef45d4bd6c67e5896c93867e
Jorge Gomez <jorge.gomez73@gmail.com>
2017-04-23 13:34:07 -0700

    Operaciones Suma resta multiplicaccion y division

--- Operaciones.py
+++ Operaciones.py
@@ -2,14 +2,23 @@ print "Programa para multiplicar, Suma, Resta, Division"
 
 print ("Selecciona un numero\n 1. Suma\n 2. Resta\n 3. Multiplicacion\n 4. Division\n")
 
-opcion = input()
-#while (opcion <=4):
+opcion=0
+
+while (opcion != 5):
+    opcion = input()
+
     if opcion == 1:
         print "Suma"
         x = input("Primer numero: ")
-        y = input ("Segundo numero: ")
+        y = input("Segundo numero: ")
         suma = int(x + y)
         print x, "+", y, "=", suma
+        continuar= input("Quieres otra operacion: ")
+
+        if continuar == "si":
+            print ("Selecciona un numero\n 1. Suma\n 2. Resta\n 3. Multiplicacion\n 4. Division\n")
+        else:
+            opcion = 5
 
     elif opcion == 2:
         print "Resta"
@@ -24,7 +33,6 @@ opcion = input()
         y = input("Segundo numero: ")
         mul = int(x * y)
         print x, "*", y, "=", mul
-        break
 
     elif opcion == 4:
         print "Division"
@@ -34,6 +42,6 @@ opcion = input()
         print x, "/", y, "=", div
 
     else:
-        print "No es opcion Valida"
-
-#print "AQuieres otra operacion"
+        print "Quieres otra operacion: "
+else:
+    print "No es opcion valida"
