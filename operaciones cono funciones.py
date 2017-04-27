05d1998c74f369dd0ddcb532ddc79888e8915224
Jorge Gomez <jorge.gomez73@gmail.com>
2017-04-26 21:10:41 -0700

    prueba tabklet

--- operaciones cono funciones.py
+++ operaciones cono funciones.py
@@ -1,48 +1,42 @@
-def num1():
-    num12 = int(input("primer numero? "))
-def num2():
-    num22 = int(input("Segundo numero? "))
+des = True
+opcion = 1
 
-print "Operaciones con funciones"
+print "Operaciones basicas"
 print ("Selecciona\n \
        1. Suma\n \
        2. Resta\n \
        3. Multiplicacion\n \
        4. Division\n")
-
 opcion = input()
 
-while opcion >= 1 and opcion <= 4:
+while opcion <= 4 and des == True:
+
     num1 = int(input("primer numero? "))
     num2 = int(input("Segundo Numero?  "))
 
     if opcion == 1:
-       # num1()
-       # num2()
         yx = [num1, num2]
         r = sum(yx)
         print num1, "+", num2, "=", r
-        opcion = 0
+        des = input("Quieres otra operacion si/no? ")
+                #if des == "si":
+                 #   des = True
 
     elif opcion == 2:
-        r = num1 - num2
-        print num1, "-", num2, "=", r
-        opcion = 0
+            r = num1 - num2
+            print num1, "-", num2, "=", r
+            opcion = 0
 
     elif opcion == 3:
-        r = num1 * num2
-        print num1, "*", num2, "=", r
-        opcion = 0
+            r = num1 * num2
+            print num1, "*", num2, "=", r
+            opcion = 0
 
     elif opcion == 4:
-        r = num1 / num2
-        print num1, "/", num2, "=", r
-        opcion = 0
+            r = num1 / num2
+            print num1, "/", num2, "=", r
+            opcion = 0
 
-    elif opcion == 0:
-        continuar = input("Quieres otra operacion si/no? ")
-        # print (input())
-        #    if continuar == "si":
-        #opcion = 0
 else:
-    print "Adios"
+    print "Gracias"
+
